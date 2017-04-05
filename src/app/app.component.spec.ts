fd4a7c1207687ec023940ea3b81f866e2d2b1c04
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-05 15:45:02 -0600

    import components used in AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { DrawerComponent } from './drawer/drawer.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        ContentComponent,
+        DrawerComponent
       ],
     }).compileComponents();
   }));
@@ -17,16 +23,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'An Angular CLI Application!'`, async(() => {
+  it(`should have as title 'Angular CLI Playground Application'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('An Angular CLI Application!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('An Angular CLI Application!');
+    expect(app.title).toEqual('Angular CLI Playground Application');
   }));
 });
